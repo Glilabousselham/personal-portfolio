f5adad2df90c97610d8d2198ec316179fe6c925a
glilabousselham <glilabousselham01@gmail.com>
2023-07-11 14:09:12 +0100

    fix urls

--- src/v1/components/ProjectDetailsModal.jsx
+++ src/v1/components/ProjectDetailsModal.jsx
@@ -33,12 +33,16 @@ const ProjectDetailsModal = ({ project, onHide }) => {
 
     const marginLeft = `-${activeImageIndex * (imageContainerRef.current?.offsetWidth ?? 0)}px`;
 
-
+    const contentRef = useRef(null)
 
     return !!project && (
-        <div className='fixed top-0 right-0 left-0 bottom-0 bg-[#00000099] flex justify-center p-2'>
+        <div onClick={({ target }) => {
+            if (!contentRef.current?.contains(target)) {
+                onHide?.()
+            }
+        }} className='fixed top-0 right-0 left-0 bottom-0 bg-[#00000099] flex justify-center overflow-y-auto p-2'>
 
-            <div className={`
+            <div ref={contentRef} className={`
                 w-full h-fit mt-10 p-4 bg-slate-800
                 md:max-w-[600px]
                 lg:max-w-[700px]
@@ -47,8 +51,12 @@ const ProjectDetailsModal = ({ project, onHide }) => {
                 <div className='flex justify-between'>
                     <div>{project.title}</div>
                     <div className='flex gap-2 text-sm'>
-                        <Button>Demo</Button>
-                        <Button>Source</Button>
+                        <Button onClick={() => {
+                            window.open(project.demo)
+                        }}>Demo</Button>
+                        <Button onClick={() => {
+                            window.open(project.source)
+                        }}>Source</Button>
                         <Button onClick={onHide}>Close</Button>
                     </div>
 
@@ -94,6 +102,13 @@ const ProjectDetailsModal = ({ project, onHide }) => {
                             <div key={i} onClick={() => setActiveImageIndex(i)} className={`p-[5px] transition-all duration-500 rounded-full ${activeImageIndex === i ? "bg-blue-600" : "bg-slate-500"}`}></div>
                         ))}
                     </div>
+                </div>
+                {/* techs */}
+                <div className='text-sm mb-2 mt-3'>Technologies :</div>
+                <div className='flex gap-2 flex-wrap text-sm'>
+                    {project.techs.map((t, i) => (
+                        <Button key={i}>{t}</Button>
+                    ))}
                 </div>
             </div>
 
